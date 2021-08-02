f242ec21546fe44ab39c92f1c853a66c64eebf11
hovaroyan <hovaroyan@gmail.com>
2021-08-02 04:10:44 +0400

    add circles and reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coloredCircles';
+  items: Array<string> =[];
+  circleDiameter!: number;
+
+
+  handleAdd(count:number) {
+    this.items.length = count;
+
+    if(count === 100){
+      this.circleDiameter = 50;
+    } else if(count === 225){
+      this.circleDiameter = 32;
+    } else {
+      this.circleDiameter = 23;
+    }
+  }
+  
+  handleReset() {
+    this.items.length = 0;
+  }
+
 }
